e8cbb5259ff78db2d3b712091a1ded7bf5bfacd6
Sivaprasad <prasadgottimukkala@gmail.com>
2024-02-17 13:27:24 +0530

    chopping cart project

--- promise/fetch.js
+++ promise/fetch.js
@@ -13,18 +13,102 @@ console.log('fetch');
 // fetch('https://jsonplaceholder.typicode.com/posts')
 //   .then((response) => response.json())
 //   .then((json) => console.log(json));
-
+const container = document.querySelector('.container');
 async function fetchPosts() {
   const data = await fetch('https://jsonplaceholder.typicode.com/posts');
   const response = await data.json();
 
   console.log(response);
+  response.map((post) => {
+    let { title } = post;
+    console.log(title);
+    const p = document.createElement('p');
+    p.classList.add('sampleclass');
+    //p.style.color = 'green';
+    //p.style.border = '2px solid black';
+    p.innerHTML = `${title};`;
+    container.append(p);
+  });
 }
-fetchPosts();
+//fetchPosts();
 //fetch('https://jsonplaceholder.typicode.com/posts/1')
-async function fetchPost() {
-  const data = await fetch('https://jsonplaceholder.typicode.com/posts/1');
+// async function fetchPost() {
+//   const data = await fetch('https://jsonplaceholder.typicode.com/posts/1');
+//   const response = await data.json();
+//   console.log(response);
+// }
+// fetchPost();
+
+// function createPosts() {
+//   fetch('https://jsonplaceholder.typicode.com/posts', {
+//     method: 'POST',
+//     body: JSON.stringify({
+//       title: 'foo',
+//       body: 'bar',
+//       userId: 1,
+//     }),
+//     headers: {
+//       'Content-type': 'application/json; charset=UTF-8',
+//     },
+//   })
+//     .then((response) => response.json())
+//     .then((json) => console.log(json));
+// }
+const apiConfigData = {
+  method: 'POST',
+  body: JSON.stringify({
+    title: 'foo',
+    body: 'bar',
+    userId: 1,
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+};
+async function createPost() {
+  const data = await fetch(
+    'https://jsonplaceholder.typicode.com/posts',
+    apiConfigData
+  );
   const response = await data.json();
   console.log(response);
 }
-fetchPost();
+createPost();
+
+function updatePost() {
+  fetch('https://jsonplaceholder.typicode.com/posts/2', {
+    method: 'PUT',
+    body: JSON.stringify({
+      id: 1,
+      title: 'foo',
+      body: 'bar',
+      userId: 1,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+}
+updatePost();
+function patchPost() {
+  fetch('https://jsonplaceholder.typicode.com/posts/2', {
+    method: 'PATCH',
+    body: JSON.stringify({
+      title: 'foo',
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+}
+patchPost();
+function deletePost() {
+  fetch('https://jsonplaceholder.typicode.com/posts/1', {
+    method: 'DELETE',
+  });
+}
+deletePost();
